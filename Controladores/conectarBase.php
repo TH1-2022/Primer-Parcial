87caa0eba598734c8a7218aae23b2375f48ec6c1
Agustin Bonilla <abonilla@netlabsHG>
2022-06-12 17:58:24 -0300

    Se arregla configuracion + conexion a base

--- Controladores/conectarBase.php
+++ Controladores/conectarBase.php
@@ -1,3 +1,25 @@
-<?php 
-    $conexionBaseDeDatos = new mysqli($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']); 
+<?php  
+    function conectar()
+    {
+        try {
+            require_once ('/home/abonilla-trabajo/Documentos/UTU/Programacion/GIT/Primer-Parcial/config.php');
+            $con = new mysqli($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']);
+            
+            mysqli_set_charset($con, "utf8");
+            
+            //echo "¡Conexión Exitosa!";
+
+            return $con;
+        } catch (mysqli_sql_exception $e) {
+            echo "¡Error!: " . $e->getMessage();
+            die();
+        }
+        
+    }
+
+    function desconectar($con)
+    {
+        mysqli_close($con);
+    }
+    $con=conectar();
 ?>
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     $config['APP_Controller'] = $_SERVER['DOCUMENT_ROOT'] . '/Controladores';
     $config['APP_Style'] = $_SERVER['DOCUMENT_ROOT'] . '/Estilos';
-    $config['DB_HOST'] = "127.0.0.1";
+    $config['DB_HOST'] = "192.168.1.28";
     $config['DB_USER'] = "admin";
     $config['DB_PASS'] = "mot_de_passe";
     $config['DB_NAME'] = "PrimerParcial_TH1_Bonilla_Agustin";

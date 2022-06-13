08331fb99b1914ff74fecca8c4653bf5bc78330c
leandro <leandrotiscornia98@gmail.com>
2022-06-13 15:14:53 -0300

    Ordenando un poquito

---
+++ App/Models/Model.class.php
@@ -0,0 +1,31 @@
+<?php
+require "utils/autoload.php";
+
+class Model
+{
+    public $DataBaseIP;
+    public $DataBaseName;
+    public $DataBaseUser;
+    public $DataBasePassword;
+    public $DataBasePort;
+    public $DataBaseName;
+    public $DataBaseConnection;
+
+    public function __construct(){
+        $this -> initializeParameters();
+        $this -> DataBaseConnectiion = new mysqli(
+            $this -> DataBaseIP, 
+            $this -> DataBaseUser, 
+            $this -> DataBasePassword, 
+            $this -> DataBaseName, 
+            $this -> DataBasePort
+        );
+    }
+    public function initializeParameters(){
+        $this -> DataBaseIP = DATABASE_IP;
+        $this -> DataBaseName = DATABASE_NAME;
+        $this -> DataBaseUser = DATABASE_USER;
+        $this -> DataBasePassword = DATABASE_PASSWORD;
+        $this -> DataBasePort = DATABASE_PORT;
+    }
+}

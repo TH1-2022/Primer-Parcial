4cf3db01ba1fb320aa97dfe9513e68f6cd7bb80b
Agustin Bonilla <abonilla@netlabsHG>
2022-06-13 12:46:18 -0300

    Se eliminan modulos para simplificar el codigo y su ejecucion

--- Vistas/AgregarCompras.php
+++
@@ -1,9 +0,0 @@
-<form action=<?=$config['Controllers'] . "/agregar.php?Listar=" . $_GET['Listar']?> method="post">
-  <p>ID Persona: <input type="text" name="idDePersona" size="35" maxlength="35"></p>
-  <p>ID Producto: <input type="text" name="idDeProducto" size="35" maxlength="35"></p>
-  <p>Fecha de compra: <input type="datetime-local" name="fechaDeCompra" size="9" maxlength="9"></p>
-  <p>
-    <input type="reset" value="Borrar">
-    <input type="submit" value="Enviar">
-  </p>
-</form>
--- Vistas/AgregarProductos.php
+++
@@ -1,9 +0,0 @@
-<form action=<?=$config['Controllers'] . "/agregar.php?Listar=" . $_GET['Listar']?> method="post">
-  <p>Nombre: <input type="text" name="nombre" size="35" maxlength="35"></p>
-  <p>Descripcion: <textarea name="descripcion" rows="10" cols="50" wrap="soft"></textarea></p>
-  <p>Stock: <input type="number" min="-1" max="999" name="stock"></p>
-  <p>
-    <input type="reset" value="Borrar">
-    <input type="submit" value="Enviar">
-  </p>
-</form>

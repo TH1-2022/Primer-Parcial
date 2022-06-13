63fb18f324a577fbec5b0f3b67754dc7d93e4532
leandro <leandrotiscornia98@gmail.com>
2022-06-13 15:29:20 -0300

    Agregado modelo de persona

---
+++ Utils/autoload.php
@@ -0,0 +1,12 @@
+
+<?php 
+    spl_autoload_register(function ($class){
+        if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/Models/$class.class.php"))
+            require $_SERVER['DOCUMENT_ROOT'] . "/Models/$class.class.php";
+    
+        if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/Controllers/$class.class.php"))
+            require $_SERVER['DOCUMENT_ROOT'] . "/Controllers/$class.class.php";
+        
+    });
+
+    require_once "config.php";

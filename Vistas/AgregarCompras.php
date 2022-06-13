f0bdd6bfcd3c5fcd08fb9eb1c59bd35e8a48feae
Agustin Bonilla <abonilla@netlabsHG>
2022-06-12 23:19:38 -0300

    Se completan los modulos de Agregar Personas/Compras/Productos

--- Vistas/AgregarCompras.php
+++ Vistas/AgregarCompras.php
@@ -1,8 +1,7 @@
-<form action=<?="Agregar.php?Listar=" . $_GET['Listar']?> method="post">
-  <p>Nombre: <input type="text" name="nombre" size="35" maxlength="35"></p>
-  <p>Apellido: <input type="text" name="nombre" size="35" maxlength="35"></p>
-  <p>Telefono: <input type="tel" name="telefono" size="9" maxlength="9"></p>
-  <p>Email: <input type="text" name="nombre" size="50" maxlength="50"></p>
+<form action=<?=$config['Controllers'] . "/Agregar.php?Listar=" . $_GET['Listar']?> method="post">
+  <p>ID Persona: <input type="text" name="nombre" size="35" maxlength="35"></p>
+  <p>ID Producto: <input type="text" name="nombre" size="35" maxlength="35"></p>
+  <p>Fecha de compra: <input type="tel" name="telefono" size="9" maxlength="9"></p>
   <p>
     <input type="reset" value="Borrar">
     <input type="submit" value="Enviar">

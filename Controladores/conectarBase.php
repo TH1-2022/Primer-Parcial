620419f6d272a39da8d7531e589463d6d4fab3ce
Agustin Bonilla <abonilla@netlabsHG>
2022-06-12 21:10:14 -0300

    Se agrega un controlador de conexion a la base de datos

--- Controladores/conectarBase.php
+++ Controladores/conectarBase.php
@@ -1,25 +1,13 @@
-<?php  
-    function conectar()
-    {
-        try {
-            require_once ('/home/abonilla-trabajo/Documentos/UTU/Programacion/GIT/Primer-Parcial/config.php');
-            $con = new mysqli($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']);
-            
-            mysqli_set_charset($con, "utf8");
-            
-            //echo "¡Conexión Exitosa!";
-
-            return $con;
-        } catch (mysqli_sql_exception $e) {
-            echo "¡Error!: " . $e->getMessage();
-            die();
-        }
+<?php   
+    try {
+        require_once ('/home/abonilla-trabajo/Documentos/UTU/Programacion/GIT/Primer-Parcial/config.php');
+        $con = new mysqli($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']);
         
+        mysqli_set_charset($con, "utf8");
+        
+        //echo "¡Conexión Exitosa!";
+    } catch (mysqli_sql_exception $e) {
+        echo "¡Error!: " . $e->getMessage();
+        die();
     }
-
-    function desconectar($con)
-    {
-        mysqli_close($con);
-    }
-    $con=conectar();
 ?>

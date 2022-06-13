c6e86ca5f5858cff37e6f7e3b22ebfa6c04ddb0e
Agustin Bonilla <abonilla@netlabsHG>
2022-06-13 18:08:13 -0300

    Se agrega configuraciones predeterminadas para correr aplicacion

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
     $config['APP_Vistas'] = $_SERVER['DOCUMENT_ROOT'] . '/Vistas';
     $config['APP_Controller'] = $_SERVER['DOCUMENT_ROOT'] . '/Controladores';
     $config['APP_Style'] = $_SERVER['DOCUMENT_ROOT'] . '/Estilos';
-    $config['DB_HOST'] = "192.168.1.28";
-    $config['DB_USER'] = "admin";
-    $config['DB_PASS'] = "mot_de_passe";
+    $config['Vistas'] = '/Vistas';
+    $config['Controllers'] = '/Controladores';
+    $config['DB_HOST'] = "";
+    $config['DB_USER'] = "";
+    $config['DB_PASS'] = "";
     $config['DB_NAME'] = "PrimerParcial_TH1_Bonilla_Agustin";

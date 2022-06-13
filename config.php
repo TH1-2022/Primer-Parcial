80348e1c7b3e36961ccfd5011818fdfd4cb7c922
SantiagoBonilla1 <85138113+SantiagoBonilla1@users.noreply.github.com>
2022-06-13 17:27:13 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    const IP_DB = "127.0.0.1";
-    const NAME_DB = "Tienda";
-    const USER_DB = "root";
-    const PASS_DB = "";
-
-    const ALTA = "agregado";
-    const BAJA = "eliminado";
-    const MODIFICACION = "modificado";

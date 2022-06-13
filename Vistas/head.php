004f8526acd6df80136a6fa3820376c7f39e44dc
Agustin Bonilla <abonilla@netlabsHG>
2022-06-13 03:09:27 -0300

    Se repara vulnerabilidad en head.php

--- Vistas/head.php
+++ Vistas/head.php
@@ -1,8 +1,4 @@
 <head>
-    <?php
-        require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php'); 
-        require_once($config['APP_Controller'] . '/conectarBase.php');
-    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

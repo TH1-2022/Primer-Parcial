1b288c807d309ac6165aa56e6120614473e48f74
Agustin Bonilla <abonilla@netlabsHG>
2022-06-13 12:46:40 -0300

    Se corrigen errores en el modulo eliminar.php

--- Controladores/eliminar.php
+++ Controladores/eliminar.php
@@ -2,10 +2,12 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . '/config.php');
     require_once ($config['APP_Controller'] . '/conectarBase.php');
     $Location = "Location: " . $config['Vistas'] . "/Listar.php?Listar=" . $_GET['Listar'];
-    if (! $_GET['Listar'] or (! $_GET['id'] and ! $_GET['Listar'] == "Compras")) {
-        header ('Location: /Vistas/404.php');
+    if (! $_GET['Listar'] or (! $_POST['id'] and $_GET['Listar'] !== "Compras")) {
+        echo "Cae aca " . $_GET['id'] . "   ";
+        //header ('Location: /Vistas/404.php');
     } elseif ((! $_GET['idP'] or ! $_GET['idC']) and $_GET['Listar'] == "Compras") {
-        header ('Location: /Vistas/404.php');
+        echo "No cae aca";
+        //header ('Location: /Vistas/404.php');
     }
 
     if ($_GET['Listar'] == "Personas") {
@@ -22,4 +24,4 @@
     }
 
     if($con -> query($sql) === TRUE ) header($Location . '&exito=eliminado');
-    else header($Location . '&exito=noeliminado&prueba='.$fecha_hora);
+    else echo json_encode(var_dump($sql)) ;//header($Location . '&exito=noeliminado');
